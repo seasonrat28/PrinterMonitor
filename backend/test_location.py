fffa0c045455830859a5435a6ed07e23e3d6168a
unknown <Administrator@PLKWSITD04.paolohospital.local>
2026-07-18 13:18:51 +0700

    chore: remove test files from repository

--- backend/test_location.py
+++
@@ -1,7 +0,0 @@
-from app.services.printer_info import get_printer_info
-
-ip = "10.119.34.21"   # เปลี่ยนเป็น IP เครื่อง Fuji
-
-info = get_printer_info(ip)
-
-print(info)
--- backend/test_service.py
+++
@@ -1,8 +0,0 @@
-from app.services.fuji_service import get_printer_info
-
-info = get_printer_info(
-    "10.119.34.21",
-    "Admin@5218"
-)
-
-print(info)
